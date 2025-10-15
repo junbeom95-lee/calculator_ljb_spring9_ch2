2f49ff809fda1ddd0362d354600e604b04c119c2
junbeom95-lee <junbeom95@gmail.com>
2025-10-15 17:00:30 +0900

    feat(v2): 계산기 v2 Calculator 클래스 calculate 메서드 구현
    1. 두 양의 정수를 받아 계산하고 반환하는 기능
    2. 매개변수: a - 첫 번째 입력받은 값
                     b - 두 번째 입력받은 값
                     operator - 연산 기호
    반환 : result  (a와 b를 연산한 결과값)

--- src/calc/Calculator.java
+++ src/calc/Calculator.java
@@ -3,11 +3,35 @@ package calc;
 public class Calculator {
     //TODO 1. 컬렉션 타입의 연산 결과를 저장하는 필드 생성
 
-    //TODO 2. calculate 기능 구현
-    public long calculate(long a, long b, char operator) {
-        long reuslt = 0;
 
-        return reuslt;
+    /**
+     * 두 양의 정수를 받아 계산하고 반환하는 기능
+     * @param a 첫 번째 입력받은 값
+     * @param b 두 번째 입력받은 값
+     * @param operator 연산 기호
+     * @return result  a와 b를 연산한 결과값
+     */
+    public long calculate(long a, long b, char operator) {
+        //1. 결과값 선언
+        long result = 0;
+
+        //2. 계산하기
+        switch (operator) {
+            case '+':   //덧셈
+                result = a + b;
+                break;
+            case '-':   //뺄셈
+                result = a - b;
+                break;
+            case '*':   //곱셈
+                result = a * b;
+                break;
+            case '/':   //나눗셈
+                result = a / b;
+        }
+
+        //3. 반환하기
+        return result;
     }
 
     //TODO
