b9e4060ebeeef99b7437c3a1df67963f332cdd41
junbeom95-lee <junbeom95@gmail.com>
2025-10-15 16:42:32 +0900

    chore(v2): Caculator 구현할 필드 및 메서드, 기능 구현 계획 주석 처리

--- src/calc/Calculator.java
+++ src/calc/Calculator.java
@@ -1,4 +1,21 @@
 package calc;
 
 public class Calculator {
+    //TODO 1. 컬렉션 타입의 연산 결과를 저장하는 필드 생성
+
+    //TODO 2. calculate 기능 구현
+    public long calculate(long a, long b, char operator) {
+        long reuslt = 0;
+
+        return reuslt;
+    }
+
+    //TODO
+    /* Getter 메서드 구현 */
+    /* Setter 메서드 구현 */
+
+    //TODO 연산 결과들 중 가장 먼저 저장된 데이터를 삭제하는 기능
+    public void removeResult() {
+        /* 구현 */
+    }
 }
