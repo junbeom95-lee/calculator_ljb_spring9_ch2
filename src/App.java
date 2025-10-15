b4d29183117a7e69fa63e4c980fa5a4e36e24311
junbeom95-lee <junbeom95@gmail.com>
2025-10-15 17:59:41 +0900

    feat(v2): 계산기에 Calculator 클래스를 적용
    1. App 클래스 계산하기 조건 변경
    1-1. 사칙 연산이 아니면 연산 기호를 잘못 입력
    1-2. 나눗셈 연산시 두 번째 정수가 0이면 안됨
    1-3. 그 외에 계산하기
    
    2. App 클래스에 Calculator 객체를 생성해서 calculator 메서드 실행
    3. 결과 출력 시 계산한 결과값들 컬렉션을 출력

--- src/App.java
+++ src/App.java
@@ -1,9 +1,13 @@
+import calc.Calculator;
+
 import java.util.Scanner;
 
 public class App {  //계산기 lv1
     public static void main(String[] args) {
 
+        //객체 생성
         Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
 
         while (true) {
             //1. 정수 입력 받기
@@ -18,26 +22,19 @@ public class App {  //계산기 lv1
 
             //3. 계산하기
             long result = 0;
-
-            if (operator == '+') {         //3-1. 계산하기 - 덧셈
-                result = a + b;
-            } else if (operator == '-') {  //3-2. 계산하기 - 뺄셈
-                result = a - b;
-            } else if (operator == '*') {  //3-3. 계산하기 - 곱셈
-                result = a * b;
-            } else if (operator == '/') {  //3.4 계산하기 - 나눗셈
-                if (b == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-                    continue;
-                }
-                result = a / b;
-            } else {
+            if( operator != '+' && operator != '-' && operator != '*' && operator != '/') {
                 System.out.println("연산 기호를 잘못입력하셨습니다.");
                 continue;
+            } else if (operator == '/' && b == 0) {
+                System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                continue;
+            } else {
+                result = calculator.calculate(a, b, operator);
             }
 
             //4. 결과 출력
             System.out.println("결과: " + result);
+            System.out.println("현재까지의 결과들 : " + calculator.getCalcResultList());
 
             //5. exit를 입력 받으면 반복 종료
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
