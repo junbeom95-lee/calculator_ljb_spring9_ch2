06a4f6aaa99dac15b08aa1c17065d78d35b2ff14
junbeom95-lee <junbeom95@gmail.com>
2025-10-15 11:49:58 +0900

    fix(v1): 계산기 v1 계산시 조건문 수정 chore(v1): 계산기 v1 주석에 순서 정렬 및 수정
    1. 입력 받은 기호는 char로 .equals 에서 == 으로 수정
    2. if, else if 조건문 수정 equals -> ==

--- src/App.java
+++ src/App.java
@@ -6,26 +6,26 @@ public class App {  //계산기 lv1
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            //정수 입력 받기
+            //1. 정수 입력 받기
             System.out.print("첫 번째 양의 정수를 입력하세요: ");
             long a = sc.nextLong();
             System.out.print("두 번째 양의 정수를 입력하세요: ");
             long b = sc.nextLong();
 
-            //사칙연산 기호 입력 받기
+            //2. 사칙연산 기호 입력 받기
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0);
 
-            //계산하기
+            //3. 계산하기
             long result = 0;
 
-            if (operator.equals("+")) {         //1-1. 계산하기 - 덧셈
+            if (operator == '+') {         //3-1. 계산하기 - 덧셈
                 result = a + b;
-            } else if (operator.equals("-")) {  //1-2. 계산하기 - 뺄셈
+            } else if (operator == '-') {  //3-2. 계산하기 - 뺄셈
                 result = a - b;
-            } else if (operator.equals("*")) {  //1-3. 계산하기 - 곱셈
+            } else if (operator == '*') {  //3-3. 계산하기 - 곱셈
                 result = a * b;
-            } else if (operator.equals("/")) {  //1.4 계산하기 - 나눗셈
+            } else if (operator == '/') {  //3.4 계산하기 - 나눗셈
                 if (b == 0) {
                     System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
                     continue;
@@ -36,10 +36,10 @@ public class App {  //계산기 lv1
                 continue;
             }
 
-            //2. 결과 출력
+            //4. 결과 출력
             System.out.println("결과: " + result);
 
-            //exit를 입력 받으면 반복 종료
+            //5. exit를 입력 받으면 반복 종료
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String exit = sc.next();
             if (exit.equals("exit")) break;
