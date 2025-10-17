d667bd06e597d97f6ac5ec28f8649382b4becd45
junbeom95-lee <junbeom95@gmail.com>
2025-10-17 20:02:51 +0900

    refactor(v2): 모듈화 및 중복 코드 제거, while -> do-while로 변경 방어적 코드 사용
    1. 숫자를 받는 부분을 메서드로 만들어서 생성 숫자가 아니면 예외 처리해서 계속 숫자로 받게 만듦
    
    2. validate 메서드 생성 : 연산기호를 잘못 받은 것을 메서드로 생성 그 외 나누기 연산자를 받았을 때 두번째 정수가 0인것도 false로 계산을 할지 정해줌
    
    3. do-while문으로 변경해서 계산 한번은 꼭 실행되도록 변경
    
    4. while문 조건을 "exit".equals(exit)로 해서 null 안전성을 더함

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import calc.Calculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {  //계산기 lv1
@@ -8,29 +9,28 @@ public class App {  //계산기 lv1
         //객체 생성
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
+        String exit = "";
 
-        while (true) {
+        do {
             //1. 정수 입력 받기
             System.out.print("첫 번째 양의 정수를 입력하세요: ");
-            long a = sc.nextLong();
+            long a = getLongInput();
             System.out.print("두 번째 양의 정수를 입력하세요: ");
-            long b = sc.nextLong();
+            long b = getLongInput();
 
             //2. 사칙연산 기호 입력 받기
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0);
 
+
+            //TODO(검증단계)
+
             //3. 계산하기
-            long result = 0;
-            if( operator != '+' && operator != '-' && operator != '*' && operator != '/') {
-                System.out.println("연산 기호를 잘못입력하셨습니다.");
+            if (!validate(operator, b)) {
                 continue;
-            } else if (operator == '/' && b == 0) {
-                System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-                continue;
-            } else {
-                result = calculator.calculate(a, b, operator);
             }
+
+            long result = calculator.calculate(a, b, operator);
 
             //4. 결과 출력
             System.out.println("결과: " + result);
@@ -38,8 +38,34 @@ public class App {  //계산기 lv1
 
             //5. exit를 입력 받으면 반복 종료
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String exit = sc.next();
-            if (exit.equals("exit")) break;
+            exit = sc.next();
+        } while ("exit".equals(exit));
+    }
+
+    private static boolean validate(char operator, long b) {
+        if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+            System.out.println("연산 기호를 잘못입력하셨습니다.");
+            return false;
+        } else if (operator == '/' && b == 0) {
+            System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+            return false;
         }
+
+        return true;
+    }
+
+    private static long getLongInput() {
+        final Scanner sc = new Scanner(System.in);
+
+        long num;
+        while (true) {
+            try {
+                num = sc.nextLong();
+                break;
+            } catch (InputMismatchException ignored) {
+                System.out.println("정수만 입력해주세요.");
+            }
+        }
+        return num;
     }
 }
