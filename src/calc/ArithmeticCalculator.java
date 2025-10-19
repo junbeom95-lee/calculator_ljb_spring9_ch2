e71cf10c39921289c67426160c58ffa7243845c5
junbeom95-lee <junbeom95@gmail.com>
2025-10-19 18:59:30 +0900

    chore(v3): v3 구현 기능 설계
    1. Enum, 제네릭, 람다 & 스트림을 이해한 계산기 만들기
    2. 실수 (double) 타입의 값을 전달 받아도 연산이 수행하도록 만들기
    3. 저장된 연산 결과들 중 Scanner로 입력받은 값보다 큰 결과값들을 출력

---
+++ src/calc/ArithmeticCalculator.java
@@ -0,0 +1,19 @@
+package calc;
+
+public class ArithmeticCalculator {
+
+    //속성
+
+    //생성자
+
+    //기능
+
+    /*
+     * TODO Enum 타입을 활용하여 사칙연산 계산기 구현
+     * TODO double 타입의 값을 전달 받아도 연산 수행 - 제네릭 사용
+     * TODO 피연산자를 여러 타입으로 받을 수 있도록 기능 확장
+     */
+    public void calculate() {
+        //수정
+    }
+}
---
+++ src/calc/OperatorType.java
@@ -0,0 +1,7 @@
+package calc;
+
+public enum OperatorType {
+
+    //TODO 연산 기호 만들기
+
+}
