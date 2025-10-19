29f6cf5c6d0d80506e566014546f47ccc24f9e72
junbeom95-lee <junbeom95@gmail.com>
2025-10-19 20:50:10 +0900

    chore(v3): 결과값들 출력 'Scanner로 받은 값보다 큰 결과값 출력' 기능 추가 설계
    1. 저장된 연산 결과들 중 Scanner로 입력 받은 값보다 큰 결과값 들을 출력 TODO 추가

--- src/calc/ArithmeticCalculator.java
+++ src/calc/ArithmeticCalculator.java
@@ -13,7 +13,10 @@ public class ArithmeticCalculator {
      * TODO double 타입의 값을 전달 받아도 연산 수행 - 제네릭 사용
      * TODO 피연산자를 여러 타입으로 받을 수 있도록 기능 확장
      */
-    public void calculate() {
+    public double calculate(double a, double b) {
         //수정
+        return 0;
     }
+
+    //TODO 저장된 연산 결과들 중 Scanner로 입력받은 값보다 큰 결과값들을 출력
 }
