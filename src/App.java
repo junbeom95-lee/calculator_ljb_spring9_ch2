6dd3bbd4fc218ca2b38ed7df6e88e83beabec5e2
junbeom95-lee <junbeom95@gmail.com>
2025-10-20 15:54:45 +0900

    refactor(v3): 각 클래스의 코드를 별도 클래스로 모듈화
    1. 입력받는 부분을 Caclulator클래스로 위임
     a. 그에 따른 valid 메서드도 Calculator클래스로 이동
    2. 결과값을 저장하는 컬렉션을 ArithmeticCalculator클래스로 이동
     a. 저장된 연산 결과들 중 최근 결과와 비교하여 나타내는 리스트 메서드(getBiggerResults) 구현
     b. ArithmeticCalculator클래스의 calculate메서드 부분 가져온 리스트에 저장하도록 addToHistory 삽입

--- src/App.java
+++ src/App.java
@@ -1,67 +1,11 @@
 import calc.Calculator;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-public class App {  //계산기 lv1
+public class App {  //계산기
     public static void main(String[] args) {
 
         //객체 생성
-        Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
-        String exit = "";
 
-        do {
-            //1. 정수 입력 받기
-            System.out.print("첫 번째 수를 입력하세요: ");
-            double a = getDoubleInput();
-            System.out.print("두 번째 수를 입력하세요: ");
-            double b = getDoubleInput();
-
-            //2. 사칙연산 기호 입력 받기
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0);
-
-            //3. 검증 및 계산하기
-            if (!validate(operator, b)) {
-                continue;
-            }
-            double result = calculator.calculate(a, b, operator);
-
-            //4. 결과 출력
-            System.out.println("결과: " + result);
-            System.out.println("현재까지의 결과들 : " + calculator.getCalcResultList());
-
-            //5. exit를 입력 받으면 반복 종료
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            exit = sc.next();
-        } while (!"exit".equals(exit));
-    }
-
-    private static boolean validate(char operator, double b) {
-        if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
-            System.out.println("연산 기호를 잘못입력하셨습니다.");
-            return false;
-        } else if (operator == '/' && b == 0) {
-            System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-            return false;
-        }
-        return true;
-    }
-
-    private static double getDoubleInput() {
-        final Scanner sc = new Scanner(System.in);
-
-        double num;
-        while (true) {
-            try {
-                num = sc.nextDouble();
-                break;
-            } catch (InputMismatchException ignored) {
-                System.out.println("숫자만 입력해주세요.");
-                sc.nextLine();  //캐시를 비워둬야함
-            }
-        }
-        return num;
+        calculator.calculate();
     }
 }
--- src/calc/Calculator.java
+++ src/calc/Calculator.java
@@ -1,51 +1,89 @@
 package calc;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Calculator {
 
     //속성
-    private final List<Double> calcResultList = new ArrayList<>();            //결과값을 저장하는 컬렉션
     ArithmeticCalculator<Double> arithmeticCalculator = new ArithmeticCalculator<>(); //계산기능을 가지고 있는 클래스
+    Scanner sc = new Scanner(System.in);
 
     //생성자
 
     //기능
     /**
-     * 두 양의 정수를 받아 계산하고 컬렉션에 저장하고 반환하는 기능
-     * @param a 첫 번째 입력받은 값
-     * @param b 두 번째 입력받은 값
-     * @param operator 연산 기호
-     * @return result  a와 b를 연산한 결과값
+     * 두 수를 입력받고 연산 기호를 입력 받아 계산하고
+     * 결과값들을 저장해서 보여주는 기능
      */
-    public double calculate(double a, double b, char operator) {
-        //1. 계산하기
-        double result = arithmeticCalculator.calculate(a, b, operator);
+    public void calculate() {
 
-        //2. 계산 이력 저장
-        addToHistory(result);
+        String exit = "";
 
-        //3. 반환하기
-        return result;
-    }
+        do {
+            //1. 정수 입력 받기
+            System.out.print("첫 번째 수를 입력하세요: ");
+            double a = getDoubleInput();
+            System.out.print("두 번째 수를 입력하세요: ");
+            double b = getDoubleInput();
 
-    /* Getter 메서드 구현 */
-    public List<Double> getCalcResultList() {
-        return calcResultList;
+            //2. 사칙연산 기호 입력 받기
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+
+            //3. 검증 및 계산하기
+            if (!validate(operator, b)) {
+                continue;
+            }
+            double result = arithmeticCalculator.calculate(a, b, operator);
+
+            //4. 결과 출력
+            System.out.println("결과: " + result);
+            System.out.println("현재까지의 결과들 : " + arithmeticCalculator.getCalcResultList());
+            System.out.println("현재까지 더 큰 수의 결과들 : " + arithmeticCalculator.getBiggerResults(result));
+
+
+            //5. exit를 입력 받으면 반복 종료
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            exit = sc.next();
+        } while (!"exit".equals(exit));
     }
 
     /**
-     * 계산 결과를 10개 이상 저장되면 첫번째꺼 삭제하기
-     * 계산 결과를 저장하기
-     * @param result 계산 결과값
+     * 연산기호를 입력받고 확인하는 메서드
+     * 1. 연산기호 / 와 정수 0 은 입력될 수 없음
+     * @param operator  연산 기호
+     * @param b 나누기 시 0이면 안되어서 매개변수로 받음
+     * @return  valid를 거쳐서 옳은 값이 오면 true
      */
-    public void addToHistory(double result) {
-        //결과를 저장하는 컬렉션 확인
-        if (calcResultList.size() >= 10) {  // 10개 이상 저장되었는지 확인
-            calcResultList.remove(0);
+    private boolean validate(char operator, double b) {
+        //연산 기호 확인
+        if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+            System.out.println("연산 기호를 잘못입력하셨습니다.");
+            return false;
+        } else if (operator == '/' && b == 0) {
+            System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+            return false;
         }
-        //결과값을 컬렉션에 저장
-        calcResultList.add(result);
+        return true;
+    }
+
+    /**
+     * 숫자를 입력받아 숫자가 아니면 숫자를 받게 하는 valid
+     * @return  입력받은 숫자
+     */
+    private double getDoubleInput() {
+        double num;
+
+        while (true) {
+            try {
+                num = sc.nextDouble();
+                break;
+            } catch (InputMismatchException ignored) {
+                System.out.println("숫자만 입력해주세요.");
+                sc.nextLine();  //캐시를 비워둬야함
+            }
+        }
+        return num;
     }
 }
