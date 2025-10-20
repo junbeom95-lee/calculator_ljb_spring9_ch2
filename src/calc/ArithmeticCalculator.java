ebb1fe1cb73b7206b222d4a05af020a2d67f8cfc
junbeom95-lee <junbeom95@gmail.com>
2025-10-20 14:32:06 +0900

    refactor(v3): Enum을 활용한 사칙연산 계산기 구현
    1. Enum 클래스를 생성하여 계산기능을 구현
     a.익명 클래스 설계로 각 상수마다 특정 동작을 구현
    2. 사칙연산으로 받은 연산기호와 상수를 연결하는 mappingChar메서드 구현 for 문으로 Enum 배열 values()로 찾을 수 있음
    3. ArithmeticCalculator클래스의 calculate메서드 Enum을 활용
    4. (chore) ArithmeticCalculator클래스의 calculate메서드 설명

--- src/calc/ArithmeticCalculator.java
+++ src/calc/ArithmeticCalculator.java
@@ -7,36 +7,28 @@ public class ArithmeticCalculator<T> {
     //생성자
 
     //기능
-
-    /*
-     * TODO Enum 타입을 활용하여 사칙연산 계산기 구현
-     * TODO double 타입의 값을 전달 받아도 연산 수행 - 제네릭 사용
-     * TODO 피연산자를 여러 타입으로 받을 수 있도록 기능 확장
+    /**
+     * 숫자 a와 b, 연산 기호를 받아 Enum에 구현된 메서드를 통해 사칙연산 계산
+     * @param a 입력받은 첫 번째 수
+     * @param b 입력받은 두 번쨰 수
+     * @param operator 사칙연산 기호
+     * @return 첫 번째 수와 두 번째 수를 사칙연산에 따라 나온 결과값
+     * @param <S> Number 객체나 하위 객체만 가능함
      */
-
-    public <T extends Number> double calculate(T a, T b, char operator) {
+    public <S extends Number> double calculate(S a, S b, char operator) {
         //1. 결과값 선언
         double result = 0;
 
-        //2. 계산하기
-        switch (operator) {
-            case '+':   //덧셈
-                result = a.doubleValue() + b.doubleValue();
-                break;
-            case '-':   //뺄셈
-                result = a.doubleValue() - b.doubleValue();
-                break;
-            case '*':   //곱셈
-                result = a.doubleValue() * b.doubleValue();
-                break;
-            case '/':   //나눗셈
-                result = a.doubleValue() / b.doubleValue();
-                break;
-        }
+        //2. 사칙연산 Enum 매핑
+        OperatorType operatorType = OperatorType.mappingChar(operator);
 
-        //3. 반환하기
+        //3. 계산하기
+        result = operatorType.apply(a.doubleValue(), b.doubleValue());
+
+        //4. 반환하기
         return result;
     }
 
     //TODO 저장된 연산 결과들 중 Scanner로 입력받은 값보다 큰 결과값들을 출력
+
 }
--- src/calc/OperatorType.java
+++ src/calc/OperatorType.java
@@ -3,5 +3,59 @@ package calc;
 public enum OperatorType {
 
     //TODO 연산 기호 만들기
+    SUM('+') {
+        @Override
+        public double apply(double a, double b) {
+            return a + b;
+        }
+    },
+    SUB('-') {
+        @Override
+        public double apply(double a, double b) {
+            return a - b;
+        }
+    },
+    MUL('*') {
+        @Override
+        public double apply(double a, double b) {
+            return a * b;
+        }
+    },
+    DIV('/') {
+        @Override
+        public double apply(double a, double b) {
+            return a / b;
+        }
+    };
 
+    //속성
+    private final char operator;
+
+    //생성자
+    OperatorType(char operator) {
+        this.operator = operator;
+    }
+
+    //기능
+    /**
+     * 문자 사칙연산을 Enum으로 바꾸는 메서드
+     * @param operator 사칙연산 ex) '+', '-', '*', '/'
+     * @return 사칙연산에 해당되는 Enum 상수
+     */
+    public static OperatorType mappingChar(char operator) {
+        for(OperatorType opType : values()) {
+            if (opType.operator == operator) {
+                return opType;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Enum 상수가 서로 다른 연산을 수행할 수 있도록 익명 클래스 형태 설계
+     * @param a 입력받은 수 a
+     * @param b 입력받은 수 b
+     * @return double형으로 a와 b를 연산한 값
+     */
+    public abstract double apply(double a, double b);
 }
